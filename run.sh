ebfdbb4003d051828c462d8a6dbaedca6e5039c7
Xi Ye <xiye17@gmail.com>
2020-09-18 01:24:07 -0400

    change ranker training setting

--- run.sh
+++ run.sh
@@ -15,11 +15,13 @@ python -u run_ranker.py \
     --logging_dir "${exp_prefix}tblogging" \
     --overwrite_output_dir \
     --learning_rate 3e-5 \
-    --num_train_epochs 10 \
+    --num_train_epochs 4 \
     --weight_decay 0.1 \
     --evaluate_during_training \
-    --logging_steps 1000 \
-    --eval_steps 50000 \
-    --save_steps 50000 \
+    --logging_steps 500 \
+    --gradient_accumulation_steps 16 \
+    --eval_steps 3000 \
+    --save_steps 3000 \
+    --warmup_steps 2000 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 2>&1 | tee "${exp_prefix}log.txt"
