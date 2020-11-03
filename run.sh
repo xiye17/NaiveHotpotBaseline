6e411f6f9e8c19762d89968af25ce6e793075568
Xi Ye <xiye17@gmail.com>
2020-11-03 15:43:31 -0600

    reconfig training

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ exp_id=$1
 exp_prefix="exps/${exp_id}/"
 
 mkdir ${exp_prefix}
+cp run.sh "${exp_prefix}/run.sh"
 
-CUDA_VISIBLE_DEVICES=0 \
+CUDA_VISIBLE_DEVICES=1,2,3 \
 python -u run_ranker.py \
     --do_train \
     --do_eval \
@@ -16,12 +17,13 @@ python -u run_ranker.py \
     --overwrite_output_dir \
     --learning_rate 3e-5 \
     --num_train_epochs 4 \
+    --evaluation_strategy "steps" \
+    --label_names "sp_labels" "ct_labels" "doc_masks" \
     --weight_decay 0.1 \
-    --evaluate_during_training \
     --logging_steps 500 \
-    --gradient_accumulation_steps 16 \
-    --eval_steps 3000 \
-    --save_steps 3000 \
-    --warmup_steps 2000 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 2>&1 | tee "${exp_prefix}log.txt"
+    --gradient_accumulation_steps 4 \
+    --eval_steps 2000 \
+    --save_steps 2000 \
+    --warmup_steps 1000 \
+    --per_device_train_batch_size 2 \
+    --per_device_eval_batch_size 2 2>&1 | tee "${exp_prefix}log.txt"
